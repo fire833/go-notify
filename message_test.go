87358fc6841f67bbc478e9aaf611c2449c29f9ff
fire833 <kttpsy@gmail.com>
2023-01-02 18:37:17 -0600

    [copyright] Updated copyright year for file headers.
    Signed-off by: Kendall Tauser <kttpsy@gmail.com>

--- message_test.go
+++ message_test.go
@@ -1,5 +1,5 @@
 /*
-*	Copyright (C) 2022  Kendall Tauser
+*	Copyright (C) 2023 Kendall Tauser
 *
 *	This program is free software; you can redistribute it and/or modify
 *	it under the terms of the GNU General Public License as published by
