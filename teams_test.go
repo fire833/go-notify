9e954853d8088e68b850621f205871f64b721e6b
fire833 <kttpsy@gmail.com>
2025-06-26 12:40:31 -0500

    chore: update copyright year

--- teams_test.go
+++ teams_test.go
@@ -1,5 +1,5 @@
 /*
-*	Copyright (C) 2024 Kendall Tauser
+*	Copyright (C) 2025 Kendall Tauser
 *
 *	This program is free software; you can redistribute it and/or modify
 *	it under the terms of the GNU General Public License as published by
